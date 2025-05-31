d0afa99c74017a5ef2682445b8515940206fc423
Francis CHIN <hellocfrancis@etik.com>
2025-05-31 23:08:42 +0200

    Updated tests: del import pytest

--- tests/test_check_data_raw.py
+++ tests/test_check_data_raw.py
@@ -1,5 +1,4 @@
 import os
-import pytest
 
 def test_check_accidents_csv():
     filename = 'data/raw/accidents_2023.csv'
--- tests/test_check_models.py
+++ tests/test_check_models.py
@@ -1,5 +1,4 @@
 import joblib
-import pytest
 
 def test_model_loading():
     model = joblib.load("models/rf_model_2023.joblib")
