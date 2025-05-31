72636be62111e1e2fe98441fb12cb32895fb75c3
Francis CHIN <hellocfrancis@etik.com>
2025-05-31 23:02:14 +0200

    Updated tests: change path

--- tests/test_check_data_raw.py
+++ tests/test_check_data_raw.py
@@ -2,6 +2,6 @@ import os
 import pytest
 
 def test_check_accidents_csv():
-    filename = '../data/raw/accidents_2023.csv'
+    filename = 'data/raw/accidents_2023.csv'
     assert os.path.exists(filename)
 
