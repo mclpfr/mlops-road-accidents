c3ce215d914bb3cf5037b7804462292b3dd00f45
Francis CHIN <hellocfrancis@etik.com>
2025-07-05 00:08:55 +0200

    Changed port

--- src/auth_api/main.py
+++ src/auth_api/main.py
@@ -7,4 +7,4 @@ app.include_router(auth_router, prefix="/auth", tags=["auth"])
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=7999)
