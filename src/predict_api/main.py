a3cccd245b6acf10488eb4e8b4bac00e442695cd
Francis CHIN <hellocfrancis@etik.com>
2025-07-05 00:07:11 +0200

    Changed port

--- src/predict_api/main.py
+++ src/predict_api/main.py
@@ -7,4 +7,4 @@ app.include_router(predict_router, prefix="/predict", tags=["predict"])
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
